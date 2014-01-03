d463913cec24891715683bd22059e9d499a6b10b
justin anderson <justin@securitymetrics.com>
2014-01-03 11:42:15 -0700

    Updated some programs

--- chap5/Exercises/futval.py
+++ chap5/Exercises/futval.py
@@ -7,13 +7,30 @@ def main():
     print("This program calculates the future value")
     print("of a 10 year investment.")
 
+    """
     principal = float(input("Enter the initial principal: "))
     apr = float(input("Enter the annual interest rate: "))
 
+    print("Year       Value")
+    print("________________")
     for i in range(10):
         principal = principal * (1 + apr)
+        print("{0:2}       ${1:0.2f}".format(i, principal))
+    """
 
-    print("The value in 10 years is: {0}".format(principal))
+    infileName = input("Enter the file name: ")
+    outfileName = input("Enter the destination file name: ")
 
+    infile = open(infileName, "r")
+    outfile = open(outfileName, "w")
+
+    for line in infile:
+        principal = principal * (1 + apr)
+        print(principal, file=outfile)
+
+    infile.close()
+    outfile.close()
+
+    print("The principal projections have been written to: {0}".format(oufileName))
 
 main()
